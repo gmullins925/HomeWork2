ca6f840e1efcabaddf2be31fad3ab8a4aa510eb6
gmullins925 <50452654+gmullins925@users.noreply.github.com>
2019-08-08 14:29:56 -0500

    Commit

---
+++ HW2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HW2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int i, count, f1 = 0, f2 = 1, f3 = 0;
+			Console.Write("Enter the Limit : ");
+			count = int.Parse(Console.ReadLine());
+
+			for (i = 0; i <= count; i++)
+			{
+				f3 = f1 + f2;
+				Console.WriteLine(Math.Max(f3,0));
+				f1 = f2;
+				f2 = f3;
+			}
+			Console.ReadLine();
+		}
+	}
+}
